e7ec6d180ad54dff8bc8516099ed86812050d1f4
Luka Woodson <llwoodson@ualr.edu>
2022-10-10 21:37:43 -0500

    Start book design

--- src/components/display/display.component.js
+++ src/components/display/display.component.js
@@ -1,13 +1,82 @@
-import { PageWrapper, Title, TitleWrapper } from "./display.styles";
+import {
+  Book,
+  BookBottom,
+  BookShadow,
+  BookSide,
+  ContentContainer,
+  PageWrapper,
+  Title,
+  TitleWrapper,
+} from "./display.styles";
 
 function DisplayComponent() {
   return (
-    <PageWrapper>
-      <TitleWrapper>
-        <Title>Ethics in the Profession - Final Project</Title>
+    <PageWrapper id={"PAGE WRAPPER"}>
+      <TitleWrapper id={"TITLE WRAPPER"}>
+        <Title id={"TITLE"}>Ethics in the Profession - Final Project</Title>
       </TitleWrapper>
+      <ContentContainer id={"container"}>
+        <Book id="book">
+          <div id="first paper">
+            <div id="page front contents">
+              <div id="intro">
+                <h2>DIARY</h2>
+                <h1>2019</h1>
+              </div>
+            </div>
+            <div id="page back"></div>
+          </div>
+
+          <BookSide id="side"></BookSide>
+          <BookBottom id="bottom"></BookBottom>
+          <BookShadow id="shadow"></BookShadow>
+        </Book>
+      </ContentContainer>
     </PageWrapper>
   );
 }
 
 export default DisplayComponent;
+
+/*
+*
+<div id="container">
+  <div class="book">
+  *
+    <div class="first paper">
+      <div class="page front contents">
+        <div class="intro">
+          <h2>DIARY</h2>
+          <h1>2019</h1>
+        </div>
+      </div>
+      <div class="page back"></div>
+    </div>
+    <div class="second paper">
+      <div class="page front contents">
+        <div id="vara-container"></div>
+      </div>
+      <div class="page back"></div>
+    </div>
+    <div class="third paper">
+      <div class="page front contents">
+        <div id="vara-container2"></div>
+      </div>
+      <div class="page back"></div>
+    </div>
+    <div class="fourth paper">
+      <div class="page last front contents">
+        <div id="vara-container3"></div>
+      </div>
+      <div class="page back"></div>
+    </div>
+    <div class="side"></div>
+    <div class="bottom"></div>
+    <div class="shadow"></div>
+
+  </div>
+</div>
+*
+*
+*
+* */
--- src/components/display/display.styles.js
+++ src/components/display/display.styles.js
@@ -18,3 +18,59 @@ export const Title = styled.div`
   color: brown;
   font-size: 30pt;
 `;
+
+export const ContentContainer = styled.div`
+  width: 60%;
+  margin: 20px auto;
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+export const BookSide = styled.div`
+  width: 3em;
+  height: 40em;
+  background: #b36060;
+  position: absolute;
+  left: -3em;
+  top: 0;
+  transform-origin: 100% 100%;
+  transform: rotateY(-90deg) rotateX(0deg);
+`;
+
+export const BookBottom = styled.div`
+  width: 25em;
+  height: 3em;
+  background: #e2e2e2;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  transform-origin: 100% 100%;
+  transform: rotateX(90deg);
+`;
+
+export const BookShadow = styled.div`
+  background: transparent;
+  transform: translateZ(-3em);
+  box-shadow: -1em 1em 0 0 #ccc3a9;
+  z-index: 1;
+`;
+
+export const Book = styled.div`
+  transform-style: preserve-3d;
+  transform: rotateX(45deg) rotateY(0deg) rotateZ(-45deg);
+  transition: transform 1s;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 10%;
+  margin: auto;
+  width: 30em;
+  height: 40em;
+`;
+
+export const Paper = styled.div`
+  transition: transform 1s, box-shadow 0.5s 0.2s;
+  transform-origin: 0 50%;
+  transform-style: preserve-3d;
+`;
