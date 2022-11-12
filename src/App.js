60f2e1bdf2438b4d2e00c2f96dc38a90297470eb
Luka Woodson <llwoodson@ualr.edu>
2022-11-11 21:30:28 -0600

    Get Vara library working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import DisplayComponent from "./components/display/display.component";
+import Vara from "vara";
+import { useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
+      {/*<div id="random"></div>*/}
       <DisplayComponent />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <script src="node_modules/vara/src/vara.min.js"></script>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
