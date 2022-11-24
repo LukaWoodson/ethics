fd3f88e98b6887524232be80f46c18b18e8f6072
Luka Woodson <llwoodson@ualr.edu>
2022-11-23 21:34:55 -0600

    Remove styled components for loader and add in css

--- src/components/loader/loader.component.js
+++ src/components/loader/loader.component.js
@@ -1,24 +1,36 @@
-import {
-  LoaderBody,
-  LoaderCircle,
-  LoaderContainer,
-  LoaderEyes,
-  LoaderHands,
-  LoaderHead,
-  LoaderText,
-} from "./loader.styles";
+import "./loader.styles.css";
+import styled from "styled-components";
+
+const LoaderContainer = styled.div`
+  background-color: #111111;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1000000;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > * {
+    transform: scale(150%);
+  }
+`;
+
 const LoaderComponent = ({ isLoading }) => {
   return (
     isLoading && (
       <LoaderContainer>
-        <LoaderCircle>
-          <LoaderHands />
-          <LoaderBody />
-          <LoaderHead>
-            <LoaderEyes />
-          </LoaderHead>
-          <LoaderText>Loading . . .</LoaderText>
-        </LoaderCircle>
+        <div className="circ">
+          <div className="hands" />
+          <div className="body" />
+          <div className="head">
+            <div className="eye"></div>
+          </div>
+        </div>
+        <div className="load">Loading...</div>
       </LoaderContainer>
     )
   );
